e3291f252b183ba3673744dff4147cd3038895a2
Brian Bycynski <high5bri@hotmail.com>
2020-06-28 02:46:19 -0400

    finished scroll project

---
+++ practice/01/app.js
@@ -0,0 +1,16 @@
+//
+//
+console.log(1234);
+
+const friend1 = 'anna';
+const friend2 = 'anna';
+const friend3 = 'anna';
+const friend4 = 'anna';
+
+const friends = ['john', 'peter', 'molly', 'josephine', 45, undefined, null];
+
+let bestFriend = friends[2];
+friends[4] = 'anna';
+console.log(friends);
+
+console.log(bestFriend);
