c67e5e120f2a2d0f920a44316e368fbb948500d6
Brian Bycynski <high5bri@hotmail.com>
2020-07-02 02:23:31 -0400

    started todo list

--- practice/01/app.js
+++ practice/01/app.js
@@ -1,92 +1,43 @@
 //
 //
-console.log(123);
+// console.log(1234);
+
+// const number = 4.56789;
+// let result = Math.floor(number);
+// const number = 4.56789;
+// let result = Math.floor(Math.random() * 3 + 1);
+
+// console.log(result);
+
+
 
 const people = [
   {
     name: 'bozo',
     age: 47,
     position: 'developer',
+    id: 1,
+    salary: 10,
   },
   {
     name: 'bozo1',
     age: 48,
     position: 'developer1',
+    id: 2,
+    salary: 20,
   },
   {
     name: 'bozo2',
     age: 49,
     position: 'developer2',
+    id: 3,
+    salary: 30,
   },
   {
     name: 'bozo3',
     age: 50,
     position: 'developer3',
+    id: 4,
+    salary: 40,
   },
 ];
-
-const youngPeople = people.filter((person) => {
-  return person.age <= 48;
-});
-
-console.log(youngPeople);
-
-const developers = people.filter((person) => person.position === 'developer');
-
-console.log(developers);
-// const ages = people.map(function (person) {
-//   // console.log(person);
-//   return person.age / 10;
-// });
-
-// console.log(ages);
-
-// const newPeople = people.map((person) => {
-//   return {
-//     firstName: person.name.toUpperCase(),
-//     oldAge: person.age + 20,
-//   };
-// });
-
-// console.log(newPeople);
-
-// const names = people.map((person) => {
-//   return `
-//     <h1>${person.name}</h1>
-//   `;
-// });
-
-// document.body.innerHTML = names.join('');
-
-// console.log(names);
-
-// function showPerson(person) {
-//   console.log(person.position.toUpperCase());
-// }
-
-// people.forEach(showPerson);
-
-// people.forEach((person) => console.log(person.position.toUpperCase()));
-
-// people.forEach((person) => {
-//   console.log(person.position.toUpperCase());
-// });
-
-// console.log(people);
-
-// const numbers = [1, 2, 3, 4, 5];
-
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// numbers.forEach((num) => {
-//   console.log(num);
-// });
-
-// console.log('--------');
-
-// numbers.map((num) => {
-//   console.log(num);
-//   // return num;
-// });
