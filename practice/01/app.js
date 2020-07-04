9e2be6be0a23162154f6b9aef5a4d7bd065d5048
Brian Bycynski <high5bri@hotmail.com>
2020-07-04 07:41:08 -0400

    finished slider

--- practice/01/app.js
+++ practice/01/app.js
@@ -2,36 +2,23 @@
 //
 // console.log(1234);
 
-/////////////////////////////////////////////////////////////////
-// const headings = document.getElementsByTagName('h2');
-// headings[0].style.color = 'red';
-// // console.log(headings.length);
+const btn = document.querySelector('.btn');
 
-// const items = document.getElementsByTagName('li');
+const heading = document.querySelector('h2');
 
-// // items.forEach((item) => {
-// //   console.log(item);
-// // });
-// items[2].style.color = 'orange';
-// const betterItems = [...items];
+const changeColors = () => {
+  let hasClass = heading.classList.contains('red');
+  if (hasClass) {
+    heading.classList.remove('red');
+  } else {
+    heading.classList.add('red');
+  }
+};
 
-// betterItems.forEach((item) => {
-//   // console.log(item);
-// });
-
-// console.log(items);
-// console.log(betterItems);
-
-////////////////////////////////////////////
-// const h1 = document.getElementById('title');
-// h1.style.color = 'red';
-// const btn = document.getElementById('btn');
-// btn.style.backgroundColor = 'blue';
-// btn.style.color = 'white';
-
-////////////////////////////////////////////
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-//   // document.body.style.backgroundColor = 'blue';
-//   document.body.classList.toggle('body');
-// });
+btn.addEventListener(
+  'click',
+  changeColors
+  // heading.classList.add('red');
+  // heading.classList.toggle('red');
+  // document.body.classList.toggle('red');
+);
