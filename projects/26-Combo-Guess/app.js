9d565b4117e4b7a7740b39d3e5d258e644d96f4f
Brian Bycynski <high5bri@hotmail.com>
2020-07-12 02:32:05 -0400

    finished section 16

--- projects/26-Combo-Guess/app.js
+++ projects/26-Combo-Guess/app.js
@@ -1,3 +1,66 @@
 //
 //
-console.log('Bozo');
+
+const gameArea = document.querySelector('.container');
+const inputContainer = document.querySelector('.input-container');
+const message = document.querySelector('.message');
+const button = document.querySelector('button');
+
+let gamePlay = false;
+let score = 0;
+
+button.addEventListener('click', function () {
+  if (!gamePlay) {
+    gamePlay = true;
+    score = 0;
+    inputContainer.innerHTML = '';
+    maker();
+    message.innerHTML = `Guess the combo`;
+    button.innerHTML = 'Check Combo';
+  } else {
+    score++;
+    message.innerHTML = `Guesses: ${score}`;
+    const numbers = document.querySelectorAll('.num');
+    let winCondition = 0;
+    numbers.forEach((num) => {
+      if (num.value == num.correct) {
+        num.style.backgroundColor = 'green';
+        num.style.color = 'white';
+        winCondition++;
+      } else {
+        let color = num.value < num.correct ? 'blue' : 'red';
+        num.style.backgroundColor = color;
+        num.style.color = 'black';
+      }
+      if (winCondition == numbers.length) {
+        gameEnd();
+      }
+    });
+  }
+});
+
+function gameEnd() {
+  if (score == 1) {
+    message.innerHTML = `You solved the combo in ${score} guess`;
+  } else {
+    message.innerHTML = `You solved the combo in ${score} guesses`;
+  }
+  gamePlay = false;
+  button.innerHTML = 'Restart Game';
+}
+
+function maker() {
+  for (let x = 0; x < 4; x++) {
+    let el = document.createElement('input');
+    el.setAttribute('type', 'number');
+    el.max = 9;
+    el.min = 0;
+    el.size = 1;
+    el.style.width = '40px';
+    el.classList.add('num');
+    el.order = x;
+    el.correct = Math.floor(Math.random() * 9 + 1);
+    el.value = 5;
+    inputContainer.append(el);
+  }
+}
