af71bbdeab1f0518b11416c158f28d55f7f83735
Brian Bycynski <high5bri@hotmail.com>
2020-07-13 02:31:54 -0400

    finished section 18

--- projects/27-Word-Scramble/app.js
+++ projects/27-Word-Scramble/app.js
@@ -1,3 +1,62 @@
 //
 //
-console.log('Bozo Boy');
+// console.log('Bozo Boy');
+
+const message = document.querySelector('.message');
+const guess = document.querySelector('input');
+
+const button = document.querySelector('button');
+const myArray = ['javascript', 'html', 'css', 'react', 'bootstrap'];
+
+let inPlay = false;
+let scramble = '';
+
+let scrambled = '';
+let score = 0;
+
+button.addEventListener('click', function () {
+  if (!inPlay) {
+    inPlay = true;
+    score = 0;
+    button.innerHTML = `Guess`;
+    guess.classList.toggle('hidden');
+    scramble = createWord();
+    scrambled = randomArray(scramble.split('')).join('');
+    message.innerHTML = `<p class="message1">${scrambled}</p>`;
+    // message.innerHTML = `<p class="message1">${scrambled} <br> ${scramble}</p>`;
+  } else {
+    let tempGuess = guess.value;
+    score++;
+    console.log(tempGuess);
+    if (tempGuess === scramble) {
+      inPlay = false;
+      if (score === 1) {
+        message.innerHTML = `Correct it was ${scramble}, it took ${score} guess`;
+      } else {
+        message.innerHTML = `Correct it was ${scramble}, it took ${score} guesses`;
+      }
+      button.innerHTML = `Start`;
+      guess.classList.toggle('hidden');
+      guess.value = '';
+    } else {
+      console.log(`Try again Bozo`);
+      // message.innerHTML = `Wrong answer bozo, try again!`;
+    }
+  }
+});
+
+function createWord() {
+  let randomIndex = Math.floor(Math.random() * myArray.length);
+  let tempWord = myArray[randomIndex];
+  return tempWord;
+}
+
+function randomArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let temp = arr[i];
+    let j = Math.floor(Math.random() * (i + 1));
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
---
+++ projects/28-Countdown-Timer/app.js
@@ -0,0 +1,37 @@
+//
+//
+// console.log('Bozo');
+
+const endDate = document.querySelector("input[name='endDate']");
+const clock = document.querySelector('.clock');
+
+endDate.addEventListener('change', function (e) {
+  e.preventDefault();
+  const temp = new Date(endDate.value);
+  console.log(temp);
+  startClock(temp);
+});
+
+function startClock(d) {
+  console.log(timeLeft(d));
+}
+
+function timeLeft(d) {
+  let currentDate = new Date();
+  // console.log(Date.parse(d));
+  // console.log(currentDate);
+  // console.log(Date.parse(currentDate));
+  let t = Date.parse(d) - Date.parse(currentDate);
+  // console.log(t);
+  let seconds = Math.floor((t / 1000) % 60);
+  let minutes = Math.floor((t / 1000 / 60) % 60);
+  let hours = Math.floor((t / ((1000 / 60) * 60 * 60)) % 24);
+  let days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    total: t,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
